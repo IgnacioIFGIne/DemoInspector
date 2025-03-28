c3e51b0641c750e3a245691d14031e3c4e21174c
IgnacioIFGIne <ignacio.delafuente@ineco.com>
2025-03-28 13:48:50 +0100

    cosas

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -32,5 +32,5 @@ def serve_static(path):
 
 
 if __name__ == "__main__":
-    app.config['DEBUG'] = True
-    app.run(port=5000)
+    port = os.environ.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
